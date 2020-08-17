3f725ab83b12d7e021ec973dc4c42246bb1b0f59
Noah Brown <n.ebrown47@gmail.com>
2020-08-16 17:02:45 -0700

    Base noise on sample rate

--- metronome.py
+++ metronome.py
@@ -4,11 +4,15 @@ sampleRate=20000.0 #Hz
 
 obj = wave.open('sound.wav', 'w')
 obj.setnchannels(1) # mono
-obj.setsampwidth(16)
+obj.setsampwidth(2)
 obj.setframerate(sampleRate)
 
-for i in range(99999):
-	value = random.randint(-32767, 32767)
+duration = 299999
+
+for i in range(299999):
+	value=0
+	if(i%sampleRate == 0):
+		value = 32767
 	data=struct.pack('<h', value)
 	obj.writeframesraw( data )
 
